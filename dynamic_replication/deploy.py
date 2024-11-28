9832d6340e745fb78350356840f91b4b6622ada5
cherifpy <simocherif2001@gmail.com>
2024-11-28 09:32:41 +0100

    changing cluster

--- dynamic_replication/deploy.py
+++ dynamic_replication/deploy.py
@@ -62,7 +62,7 @@ if True:
     config = Configuration(
         config_file_path = PATH_TO_CONFIG_FILE,
         nb_nodes=11,
-        cluster='paradoxe',
+        cluster='ecotype',
         latency='100ms'
     )
     
